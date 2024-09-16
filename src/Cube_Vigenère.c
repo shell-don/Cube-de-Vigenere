efedf62176fe30721667f057dac615f00ee5cd3d
Mathis <120995618+shell-don@users.noreply.github.com>
2024-09-17 00:06:53 +0200

    Update Cube_Vigenère.c

--- src/Cube_Vigenère.c
+++ src/Cube_Vigenère.c
@@ -101,7 +101,7 @@ int main() {
     
 	setlocale(LC_ALL, "") ;
 
-  size_t buffer_size = 1024 ; 
+    	size_t buffer_size = 1024 ; 
 	char Choice[3] ; 
 	int lignes, Colonnes ;
 
@@ -111,81 +111,81 @@ int main() {
 	if (*(&Choice[0]) != '1' && *(&Choice[0]) != '2') {
         	printf("Choix invalide.\n") ;
         	return 1 ; 
-  }
+    	}
 	
-  // Allocation de la mémoire pour le message
-  wchar_t* buffer = (wchar_t*)calloc(buffer_size, sizeof(wchar_t)) ;
+    	// Allocation de la mémoire pour le message
+   	wchar_t* buffer = (wchar_t*)calloc(buffer_size, sizeof(wchar_t)) ;
 	if (buffer == NULL) {
         	fwprintf(stderr, L"Erreur d'allocation de mémoire pour le tampon\n") ;
         	return 1 ;
-  }
+    	}
 
-  // Lire le message
-  fprintf(stdout, "Entrez le message : \n") ;
-  if (fgetws(buffer, buffer_size, stdin) == NULL) {
+    	// Lire le message
+    	fprintf(stdout, "Entrez le message : \n") ;
+    	if (fgetws(buffer, buffer_size, stdin) == NULL) {
         	fwprintf(stderr, L"Erreur de lecture de l'entrée\n") ;
         	free(buffer) ;
         	return 1 ;
-  }
+    	}
 	size_t size_message = wcslen(buffer) ;
 
 	// Supprimer le caractère de nouvelle ligne s'il est présent
-  if (size_message > 0 && buffer[size_message - 1] == L'\n') {
+    	if (size_message > 0 && buffer[size_message - 1] == L'\n') {
         	buffer[size_message - 1] = L'\0' ;
         	size_message-- ;
-  }
+    	}
 
 	if (Miller_Rabin(size_message, 10)) {
     		fprintf(stderr, "Erreur : la taille du message est un nombre premier\n") ;
-		    free(buffer) ;
+		free(buffer) ;
     		return 1 ;
-  }
+  	}
     	
 	// Allocation pour le message
-  wchar_t* message = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
+    	wchar_t* message = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
     	if (message == NULL) {
         	fwprintf(stderr, L"Erreur d'allocation de mémoire pour le message\n") ;
         	free(buffer) ;
         	return 1 ;
     	}
-  wcscpy(message, buffer) ;
+    	wcscpy(message, buffer) ;
 
-  // Allocation pour le message chiffré
-  wchar_t* cipher_message = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
-  if (cipher_message == NULL) {
+    	// Allocation pour le message chiffré
+    	wchar_t* cipher_message = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
+    	if (cipher_message == NULL) {
         	fwprintf(stderr, L"Erreur d'allocation de mémoire pour le message chiffré\n") ;
         	free(message) ;
         	free(buffer) ;
         	return 1 ;
-  }
+    	}
 
-  // Allocation pour la clé répétée
-  wchar_t* cle = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
-  if (cle == NULL) {
+    	// Allocation pour la clé répétée
+    	wchar_t* cle = (wchar_t*)calloc(size_message + 1, sizeof(wchar_t)) ;
+    	if (cle == NULL) {
         	fwprintf(stderr, L"Erreur d'allocation de mémoire pour la clé\n") ;
         	free(cipher_message) ;
         	free(message) ;
         	free(buffer) ;
         	return 1 ;
-  }
+    	}
 
-  // Lire la clé
-  fwprintf(stdout, L"Entrez la clé : \n") ;
-  if (fgetws(buffer, buffer_size, stdin) == NULL) {
+    	// Lire la clé
+    	fwprintf(stdout, L"Entrez la clé : \n") ;
+    	if (fgetws(buffer, buffer_size, stdin) == NULL) {
         	fwprintf(stderr, L"Erreur de lecture de l'entrée pour la clé\n") ;
         	free(cle) ;
         	free(cipher_message) ;
         	free(message) ; 
         	free(buffer) ;
         	return 1 ;
-  }
+    	}
 	size_t size_key = wcslen(buffer) ;
     	
-  // Supprimer le caractère de nouvelle ligne s'il est présent
-  if (size_key > 0 && buffer[size_key - 1] == L'\n') {
+    	// Supprimer le caractère de nouvelle ligne s'il est présent
+    	if (size_key > 0 && buffer[size_key - 1] == L'\n') {
         	buffer[size_key - 1] = L'\0' ;
         	size_key-- ;
-  }
+    	}
 	repeatKey(buffer, size_message, cle) ;
 	size_key = wcslen(cle) ;
 
@@ -200,66 +200,69 @@ int main() {
 
 	// transposition cle
 	rectangle_proche(size_message, &lignes, &Colonnes) ;
-  wchar_t Tableau[lignes][Colonnes] ; 
-  int k = 0 ;
-  for (int i = 0 ; i < lignes ; i++) {
-      for (int j = 0 ; j < Colonnes ; j++) {
+    	wchar_t Tableau[lignes][Colonnes] ; 
+    	int k = 0 ;
+    	for (int i = 0 ; i < lignes ; i++) {
+      		for (int j = 0 ; j < Colonnes ; j++) {
         		Tableau[i][j] = cle[k] ; k++ ;
-      } 
-  } 
+      		} 
+    	} 
 	int p = 0 ;
-  for (int j = 0 ; j < Colonnes ; j++) {
-      for (int i = 0 ; i < lignes ; i++) {
+    	for (int j = 0 ; j < Colonnes ; j++) {
+      		for (int i = 0 ; i < lignes ; i++) {
         		cle[p] = Tableau[i][j] ; p++ ;
-      } 
-  } 
+      		} 
+    	} 
 	cle[size_message] = L'\0' ;
 
-  free(buffer) ; // cipher_message, cle, message
+    	free(buffer) ; // cipher_message, cle, message
 
 	if (*(&Choice[0]) == '1') {
 
-  // Chiffrement du message
+    	// Chiffrement du message
 	for (size_t i = 0 ; i < size_message ; i++) {
     		
-	  int x = (int)message[i] ;
-	  int key_val = (int)cle[i] ; 
+		int x = (int)message[i] ;
+		int key_val = (int)cle[i] ; 
 		int y = log_discret(2, key_val, 1114083) ;
-	  cipher_message[i] = (wchar_t)(((x + y) + 3) % 0x10FFFF) ;
+		
+    		cipher_message[i] = (wchar_t)(((x + y) + 3) % 0x10FFFF) ;
 
 	}
 	cipher_message[size_message] = L'\0' ;
 
-  // Affichage du message chiffré
-  fwprintf(stdout, L"%ls", cipher_message) ;
+    	// Affichage du message chiffré
+    	fwprintf(stdout, L"%ls", cipher_message) ;
 
-  // Libération de la mémoire
-  free(cle) ;
-  free(cipher_message) ;
-  free(message) ;
+    	// Libération de la mémoire
+    	free(cle) ;
+    	free(cipher_message) ;
+    	free(message) ;
+
     	return 0 ;
 	}
 
 	if (*(&Choice[0]) == '2') {
 
 	// Déhiffrement du message
-  for (size_t i = 0 ; i < size_message ; i++) {
+    	for (size_t i = 0 ; i < size_message ; i++) {
     		int x = (int)message[i] ;
-   	   	int key_val = (int)cle[i] ;
-		    int y = log_discret(2, key_val, 1114083) ;
+   	 	int key_val = (int)cle[i] ;
+		int y = log_discret(2, key_val, 1114083) ;
 		
-		    cipher_message[i] = (wchar_t)(((x - y) - 3) % 0x10FFFF) ;
+		cipher_message[i] = (wchar_t)(((x - y) - 3) % 0x10FFFF) ;
 	}
-  cipher_message[size_message] = L'\0' ; // Assurer la terminaison de la chaîne
+    	cipher_message[size_message] = L'\0' ; // Assurer la terminaison de la chaîne
 
-  // Affichage du message chiffré
-  fwprintf(stdout, L"%ls", cipher_message) ;
+    	// Affichage du message chiffré
+    	fwprintf(stdout, L"%ls", cipher_message) ;
 
-  // Libération de la mémoire
-  free(cle) ;
-  free(cipher_message) ;
-  free(message) ;
-  return 0 ;
+    	// Libération de la mémoire
+    	free(cle) ;
+    	free(cipher_message) ;
+    	free(message) ;
+
+    	return 0 ;
 	}
 
 	return 1 ;
